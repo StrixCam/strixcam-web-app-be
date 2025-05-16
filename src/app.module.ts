93f0d98928c1826699a54f80b28fce2ba544675c
Raul Santamaria <raulsantamariao@hotmail.com>
2025-05-15 21:31:07 -0500

    Add modules for overlays, matches, teams, and sports to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OverlayModule } from './overlays/overlays.module';
+import { MatchModule } from './matches/matches.module';
+import { TeamsModule } from './teams/teams.module';
+import { SportsModule } from './sports/sports.module';
 
 @Module({
-  imports: [],
+  imports: [OverlayModule, MatchModule, TeamsModule, SportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
